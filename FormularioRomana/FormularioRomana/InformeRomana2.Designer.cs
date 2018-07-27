4d9dcc81bb585aec08b63d5596497c284bffc0e4
hfsanchez <hecfrasan@gmail.com>
2018-07-27 18:24:24 -0400

    Actualizacion

--- FormularioRomana/FormularioRomana/InformeRomana2.Designer.cs
+++ FormularioRomana/FormularioRomana/InformeRomana2.Designer.cs
@@ -81,7 +81,7 @@
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "FormularioRomana.InformeRomana2.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(697, 560);
+            this.reportViewer1.Size = new System.Drawing.Size(682, 560);
             this.reportViewer1.TabIndex = 0;
             this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
             // 
@@ -101,7 +101,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 560);
+            this.ClientSize = new System.Drawing.Size(682, 560);
             this.Controls.Add(this.reportViewer1);
             this.Name = "InformeRomana2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
